e910ca15fc02952f37c6bd6377f0ec3a83a733e5
Tseganesh Yifru <tseganeshyifruberta>
2023-12-12 19:51:01 +0300

    fix build error

--- swe-hotel-booking-management-system-web/pages/rooms/index.tsx
+++ swe-hotel-booking-management-system-web/pages/rooms/index.tsx
@@ -1,9 +1,13 @@
 import TestimonalsCard from "@/components/home/TestimonalsCard";
 import RoomCard from "@/components/room/RoomCard";
+import RoomIntro from "@/components/room/RoomIntro";
 
 function Rooms() {
   return (
     <div>
+      <div>
+        <RoomIntro />
+      </div>
       <div>
         <div className="flex justify-center p-4">
           <RoomCard />
